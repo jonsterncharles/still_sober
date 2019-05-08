f0e1881e90c770234e761ffa516c282c8ed61948
Glitch (still-sober) <none>
2019-05-08 17:20:36 +0000

    Added scrolling and changed layout and font

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-A chat app built with express, socket.io, jquery, JSON, html2canvas, and canvas2image.
+A chat app built with node.js, express, socket.io, jquery, JSON, html2canvas, and canvas2image.
 
-A place for sober people to chat.
+Inspired by a glitch project by Sylvi Scahill and the hello-express project framework provided by Glitch. 
+
+An anonymous place for sober people to express themselves and recieve feedback. When used by many it provides an endless stream of consciousness in the hope that it will help people stay sober until their next meeting. 
+
+Planning to add screenshot funcitonality on button click:
+
+  -"If you see something of value that will get you through until your next meeting, whether it is great advice, a joke that will keep you smiling, or a story that invokes hope, click "Save Inspiration" and a screenshot of the chat will download onto your device."
+  
+  -<button id="btnSave">
+        Save Inspiration
+    </button>
--- public/client.js
+++ public/client.js
@@ -1,14 +1,47 @@
+//chat function
+var io
 var socket = io();
-
-
 function ShareWithUs(){
 socket.emit('sendMessage' , {message: $('textarea').val()} )
-  
 console.log("i am here")
 }
 socket.on('NewMessage', function(data){
-  $('body').append("<p>"  +data.message+ " </p>")
+$('.chat').append("<p>"  +data.message+ "</p>")
 })
-
-
 ShareWithUs()
+
+//scroll to bottom on chat submit
+
+$(".bottom").click(function() {
+  $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+  return false;
+});
+
+//scroll to top on button click
+
+$(".top").click(function() {
+  $("html, body").animate({ scrollTop: $(document).height(0) }, "slow");
+  return false;
+});
+
+//screenshot function
+// DOESNT WORK YET
+var c = document.getElementById('the_canvas_element_id');
+var t = c.getContext('2d');
+/* then use the canvas 2D drawing functions to add text, etc. for the result */
+window.open('', document.getElementById('the_canvas_element_id').toDataURL());
+
+$(function() { 
+    $("#btnSave").click(function() { 
+        html2canvas($("#widget"), {
+            onrendered: function(canvas) {
+                theCanvas = canvas;
+
+
+                canvas.toBlob(function(blob) {
+                    saveAs(blob, "Dashboard.png"); 
+                });
+            }
+        });
+    });
+});
